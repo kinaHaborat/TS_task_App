3ddd43102757a5b0bd946a12fcd24969120142e3
Tiago Baptista <tbaptista@live.com.pt>
2023-04-17 22:43:58 +0100

    add final version of the app

--- dest/index.js
+++ dest/index.js
@@ -1,8 +1,8 @@
 "use strict";
 class Task {
-    constructor(title, id, date, order) {
+    constructor(title, date, order) {
         this.title = title;
-        this.id = id;
+        this.id = Task.currentId++;
         this.date = date;
         this.order = order;
     }
@@ -10,22 +10,34 @@ class Task {
         return `${this.title} ${this.id.toString()} ${this.date.toLocaleString()} ${this.order.toString()}`;
     }
 }
+Task.currentId = 0;
 let taskList = [];
 function printTaskList() {
-    taskList = JSON.parse(localStorage.taskList);
-    console.log(taskList);
+    taskList = JSON === null || JSON === void 0 ? void 0 : JSON.parse(localStorage.taskList);
     const taskListElem = document.getElementById("taskList");
     taskList === null || taskList === void 0 ? void 0 : taskList.forEach((task) => {
-        let task_ = new Task(task.title, task.id, task.date, task.order);
+        let task_ = new Task(task.title, task.date, task.order);
         let li = document.createElement("li");
+        let button = document.createElement("button");
+        button.setAttribute("id", `${task.id}`);
+        button.setAttribute("onClick", `removeTask(${task.id})`);
+        button.setAttribute("class", "delBtn button ");
+        button.innerHTML = " X ";
         li.innerHTML = task_.convertToString();
         taskListElem === null || taskListElem === void 0 ? void 0 : taskListElem.appendChild(li);
+        taskListElem === null || taskListElem === void 0 ? void 0 : taskListElem.appendChild(button);
     });
 }
 function createTask() {
     const inputElem = document.getElementById("title");
     const titleValue = inputElem === null || inputElem === void 0 ? void 0 : inputElem.value;
-    taskList.push(new Task(titleValue, 1, new Date(), 1));
+    taskList.push(new Task(titleValue, new Date(), 1));
     localStorage.taskList = JSON.stringify(taskList);
+}
+function removeTask(taskId) {
+    taskList = JSON.parse(localStorage.taskList);
+    taskList = taskList.filter((taskItem) => taskItem.id !== taskId);
+    localStorage.taskList = JSON.stringify(taskList);
+    location.reload();
 }
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 class Task {
+  static currentId = 0;
   title: string;
   id: number;
   date: Date;
   order: number;
 
-  constructor(title: string, id: number, date: Date, order: number) {
+  constructor(title: string, date: Date, order: number) {
     this.title = title;
-    this.id = id;
+    this.id = Task.currentId++;
     this.date = date;
     this.order = order;
   }
@@ -21,23 +22,34 @@ class Task {
 let taskList: Task[] = [];
 
 // LocalStorage
-
 function printTaskList() {
-  taskList = JSON.parse(localStorage.taskList);
-  console.log(taskList);
+  taskList = JSON?.parse(localStorage.taskList);
   const taskListElem = document.getElementById("taskList");
   taskList?.forEach((task: any) => {
-    let task_ = new Task(task.title, task.id, task.date, task.order);
+    let task_ = new Task(task.title, task.date, task.order);
     let li = document.createElement("li");
+    let button = document.createElement("button") as HTMLButtonElement;
+    button.setAttribute("id", `${task.id}`);
+    button.setAttribute("onClick", `removeTask(${task.id})`);
+    button.setAttribute("class", "delBtn button ");
+    button.innerHTML = " X ";
     li.innerHTML = task_.convertToString();
     taskListElem?.appendChild(li);
+    taskListElem?.appendChild(button);
   });
 }
 
-// Create Task - its printing the full array for now
+// Create Task
 function createTask() {
   const inputElem = document.getElementById("title") as HTMLInputElement;
   const titleValue = inputElem?.value;
-  taskList.push(new Task(titleValue, 1, new Date(), 1));
+  taskList.push(new Task(titleValue, new Date(), 1));
   localStorage.taskList = JSON.stringify(taskList);
+}
+//Remove Task based on the specified item Id
+function removeTask(taskId: number) {
+  taskList = JSON.parse(localStorage.taskList);
+  taskList = taskList.filter((taskItem) => taskItem.id !== taskId);
+  localStorage.taskList = JSON.stringify(taskList);
+  location.reload();
 }
