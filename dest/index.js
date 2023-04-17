7a3e64cbb43687b6a2ea7377d942f4cae6fb3428
Tiago Baptista <tbaptista@live.com.pt>
2023-04-17 08:59:49 +0100

    add the all the fille for the app - Not finished

--- dest/index.js
+++ dest/index.js
@@ -1,3 +1,31 @@
 "use strict";
-let user = [1, "mosh"];
+class Task {
+    constructor(title, id, date, order) {
+        this.title = title;
+        this.id = id;
+        this.date = date;
+        this.order = order;
+    }
+    convertToString() {
+        return `${this.title} ${this.id.toString()} ${this.date.toLocaleString()} ${this.order.toString()}`;
+    }
+}
+let taskList = [];
+function printTaskList() {
+    taskList = JSON.parse(localStorage.taskList);
+    console.log(taskList);
+    const taskListElem = document.getElementById("taskList");
+    taskList === null || taskList === void 0 ? void 0 : taskList.forEach((task) => {
+        let task_ = new Task(task.title, task.id, task.date, task.order);
+        let li = document.createElement("li");
+        li.innerHTML = task_.convertToString();
+        taskListElem === null || taskListElem === void 0 ? void 0 : taskListElem.appendChild(li);
+    });
+}
+function createTask() {
+    const inputElem = document.getElementById("title");
+    const titleValue = inputElem === null || inputElem === void 0 ? void 0 : inputElem.value;
+    taskList.push(new Task(titleValue, 1, new Date(), 1));
+    localStorage.taskList = JSON.stringify(taskList);
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,43 @@
-let user: [number, string] = [1, "mosh"];
-user.push("pew");
-console.log(user);
+class Task {
+  title: string;
+  id: number;
+  date: Date;
+  order: number;
+
+  constructor(title: string, id: number, date: Date, order: number) {
+    this.title = title;
+    this.id = id;
+    this.date = date;
+    this.order = order;
+  }
+  convertToString(): string {
+    return `${
+      this.title
+    } ${this.id.toString()} ${this.date.toLocaleString()} ${this.order.toString()}`;
+  }
+}
+
+// Create an Tasks Array
+let taskList: Task[] = [];
+
+// LocalStorage
+
+function printTaskList() {
+  taskList = JSON.parse(localStorage.taskList);
+  console.log(taskList);
+  const taskListElem = document.getElementById("taskList");
+  taskList?.forEach((task: any) => {
+    let task_ = new Task(task.title, task.id, task.date, task.order);
+    let li = document.createElement("li");
+    li.innerHTML = task_.convertToString();
+    taskListElem?.appendChild(li);
+  });
+}
+
+// Create Task - its printing the full array for now
+function createTask() {
+  const inputElem = document.getElementById("title") as HTMLInputElement;
+  const titleValue = inputElem?.value;
+  taskList.push(new Task(titleValue, 1, new Date(), 1));
+  localStorage.taskList = JSON.stringify(taskList);
+}
